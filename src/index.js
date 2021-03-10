895c457accc3f07c3e1783f2840a6e70f6a8222f
noxolica <angelokn14@gmail.com>
2021-03-09 22:20:19 -0800

    catch all non-existent routes and return a 404 error

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const routes = require('./routes');
 const app = express();
 app.use(express.json());
 app.use('/', routes);
+app.use((req, res) => {
+  res.json({
+    error: {
+      message: 'route not found',
+      status: 404,
+    },
+  });
+});
 
 app.listen(process.env.PORT, async () => {
   try {
