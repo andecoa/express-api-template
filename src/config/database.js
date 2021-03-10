93619b7b2a4a56526cadeab483dd7c1f03e0f85e
noxolica <angelokn14@gmail.com>
2021-03-09 17:18:28 -0800

    set up pino logger

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
+const logger = require('../utils/logger');
 
 const initDatabase = async () => {
   mongoose.connection
     .on('error', () => {
-      console.log(
+      logger.error(
         `Failed to connect to app database. Retrying in 10 seconds!`,
       );
       setTimeout(initDatabase, 10000);
     })
-    .once('open', () => console.log('Connected to database!'));
+    .once('open', () => logger.info('Connected to database!'));
 
   if (!process.env.MONGO_SRV) {
     throw new Error('Please set $MONGO_SRV');
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const database = require('./config/database');
+const logger = require('./utils/logger');
 
 const app = express();
 
 app.listen(process.env.PORT, async () => {
   try {
     await database();
+    logger.info(`Started app on port ${process.env.PORT}`);
   } catch (err) {
-    console.log(err);
+    logger.error(err);
   }
 });
