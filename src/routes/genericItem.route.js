c6d2a949148078ce9a246433510c411c9c8ae646
noxolica <angelokn14@gmail.com>
2021-03-09 22:21:20 -0800

    add validation logic to routes

--- src/routes/genericItem.route.js
+++ src/routes/genericItem.route.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const genericItemController = require('../controllers/genericItem.controller');
+const validate = require('../middlware/validations/genericItem.validations');
 
-router.post('/', [genericItemController.createGenericItem]);
+router.post('/', [
+  validate.validateGenericItemBody,
+  genericItemController.createGenericItem,
+]);
 
-router.get('/:genericItemId', [genericItemController.getGenericItem]);
+router.get('/:genericItemId', [
+  validate.validateGenericItemId,
+  genericItemController.getGenericItem,
+]);
 
 module.exports = router;
